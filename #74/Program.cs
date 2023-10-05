ba70a325b7d5d63857c132022b5490cc91202a55
dstomasfigueroa@hotmail.com <dstomasfigueroa@hotmail.com>
2023-10-04 22:36:01 -0300

    Agregar archivos de proyecto.

---
+++ #74/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Ejercicio
+{
+    public static void Main()
+    {
+
+
+        Stack<string> Pila = new Stack<string>();
+
+        while (true)
+        {
+            Console.WriteLine("Escriba las cadenas de texto que desee almacenar en la Pila");
+            Console.WriteLine("o introduzca una cadena vacia para recibir lo almacenado en la Pila");
+            Console.WriteLine("");
+
+            string datoEnPila = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            if (datoEnPila == "")
+            {
+                MostrarPila(Pila);
+                break;
+            }
+            else
+            {
+                Pila.Push(datoEnPila);
+            }
+        }
+
+        static void MostrarPila(Stack<string> Pila)
+        {
+            while (Pila.Count > 0)
+            {
+                string dato = (string)Pila.Pop();
+
+                Console.WriteLine(dato);
+            }
+        }
+
+    }
+}
